00f9b99041814aa236cd36d20878b581547e232f
Anish Dhiman <82077105+anish612@users.noreply.github.com>
2023-04-17 12:44:53 +0530

    Update s3-api-lamda.py

--- Anish/Daily status/s3-api-lamda.py
+++ Anish/Daily status/s3-api-lamda.py
@@ -4,16 +4,15 @@ import boto3
 s3 = boto3.client("s3")
 
 def lambda_handler(event, context):
-    bucket = "test-api-lamda-bucket1"
-    key = "index.html"
+    bucket = event ['bucket']
+    key = event ['key']
     
     try:
         data = s3.get_object(Bucket=bucket , Key = key)
         json_data = data["Body"].read()
-        data = json.load(json_data)
         return {
             "response_code" : 200,
-            "data" = data
+            "data" : str(json_data)
         }
     except Exception as e:
         print(e)
