b5e318aa5f1c3f2774ce5c2559500e4d8b8a57d4
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2024-02-03 21:11:36 +0530

    Delete Anuradha/Account/myproject/accounts directory

--- Anuradha/Account/myproject/accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Account(models.Model):
-    id = models.AutoField(primary_key=True)
-    configuration = models.JSONField()
-    account_type = models.CharField(max_length=10)
